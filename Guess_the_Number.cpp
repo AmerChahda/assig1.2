2307832dc7f2a20464ccc26a83e1451a72c5943b
AmerChahda <amer.chahda@sigma.se>
2018-07-31 11:52:18 +0200

    Readjust the code in Assignment 1.2 as Allman Indent style

--- Guess_the_Number.cpp
+++ Guess_the_Number.cpp
@@ -8,31 +8,46 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-using namespace std;
 
-int main(int argc, char **argv) {
-    int user_number=0,sys_number;
+
+int main(int argc, char **argv)
+{
+    int user_number = 0;
+    int sys_number;
     char repeat;
-    cout<<"\t\tWellcome to Guessing Number Game\n\t";
-    do{
-    sys_number=rand()%100+1;
-    while(1){
-	cout<<"Enter a number between 1&100 and try to guess the number which I choose.\n";
-	cin>>user_number;
-	if (user_number==sys_number){
-	    cout<<"\tThat is right YOU GET IT.\n";
-	    break;
-	}
-	else if(user_number<sys_number){
-	    cout<<"Your number is lower than mine!!!.... Pls. try again....\n ";
-	}
-	else
-	    cout<<"Your number is larger than mine!!!.... Pls. try again....\n ";
+
+    std::cout << "\t\tWellcome to Guessing Number Game\n\t";
+
+    do
+    {
+		sys_number = rand()%100+1;
+
+		while (1)
+		{
+			std::cout << "Enter a number between 1&100 and try to guess the number which I choose.\n";
+			std::cin >> user_number;
+
+			if (user_number == sys_number)
+			{
+				std::cout << "\tThat is right YOU GET IT.\n";
+				break;
+			}
+			else if (user_number < sys_number)
+			{
+				std::cout << "Your number is lower than mine!!!.... Pls. try again....\n ";
+			}
+			else
+				std::cout << "Your number is larger than mine!!!.... Pls. try again....\n ";
+		}
+
+		std::cout << "Do you want to continue playing press 'y' or 'Y'\n";
+		std::cin >> repeat;
+
     }
-    cout<<"Do you want to continue playing press 'y' or 'Y'\n";
-    cin>>repeat;
-    }while ((repeat=='Y')||(repeat=='y'));
-    cout<<"See you later!!!";
+    while ((repeat == 'Y') || (repeat == 'y'));
+
+    std::cout << "See you later!!!";
+
     return 0;
 }
 
